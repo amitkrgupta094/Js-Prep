702fb500bf634849ecb41e8cea7eea00d247d8a5
Amit Gupta <33304344+Amilight@users.noreply.github.com>
2021-06-18 22:43:54 +0530

    Create Js_deep_dive_01.js
    Addition of Module 1 of deep dive

---
+++ Js_deep_dive_01.js
@@ -0,0 +1,54 @@
+// Introduction
+
+// Lecture 1 - Introduction
+/*
+  - Kyle talks about how he got into teaching.
+  - Connet: @getify.
+  - Learn JavaScript Deeply
+    - kyle talks about an example including x++ / ++x.
+    - Kyle says it's essential for a JS developer to understand and have correct mental developers.
+    - Read the Spec:  https://262.ecma-international.org/11.0/
+    - Deck: 1-4
+*/
+
+
+// Lecture 2 - Understanding your code
+/*
+  Reading the Spec -
+    - It is challenging but keep going on these as these states how certain things works inside of
+    JavaScript.
+    - MDN is not always right so go for EcmaScript Spec.
+    - Having correct mental model leads us less buddy software.
+    - Deck: 7-10
+*/
+
+
+// Lecture 3 - Course Overview
+/*
+ 3 Pillars of Js:
+ 
+ Types System:
+   - Primitive Types
+   - Abstract Operations
+   - Coercion
+   - Equality
+   - TypeScript, Flow etc
+   
+ Scope System:
+   - Nested Scope
+   - Hoisting
+   - Closure
+   - Modules
+   
+Objects (Oriented):
+  - this
+  - class {}
+  - Prototypes
+  - OO vs OLOO
+  
+
+- Kyle says class keyword is not same as in other classical programming languages.
+
+  
+ Deck: 11 - 13
+*/
