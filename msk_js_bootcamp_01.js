ccc2c05e8cb51cfdb844cbfc9d99d6da96940934
Amit Gupta <33304344+Amilight@users.noreply.github.com>
2021-06-19 11:05:43 +0530

    Create msk_js_bootcamp_01.js
    Msk-bootcamp-notes

---
+++ msk_js_bootcamp_01.js
@@ -0,0 +1,115 @@
+// Concept 1: Course Agenda
+/*
+
+ *********Part 1***************
+ Introduction to JavaScript
+ Js Runtime, Compiler passes and hoisting
+ Whar are prototypes in Javascript?
+ Datatypes, conditions & loops
+ JavaScript Objects, properties & methods
+ Functions in javascript
+ IIFE pattern
+ What is the event loop?
+ 
+ *********Part 2******************
+ All Static & prototype string methods
+ All Array methods 
+ Static & Dynamic importing
+ Callbacks and promises
+ Overcoming the callback hell
+ Closures
+ 
+*/
+
+
+// Concept 2: Bio of Author
+// Manoj Satish Kumar - Sr. JavaScript Developer 
+// As Js developer - We are UI developer. We act as a product manager because we drive product/roadmap. Oragabize your life.
+
+
+
+
+// Concept 3 - Component Based Design
+/*
+ - Break down into multiple components and use it.
+ - It scales easily.
+ - Monolith design : break it down into small parts and use component based design. 
+ - Scale in UI
+   - Maintaining a project
+   - 
+   
+- Tip:
+  - Use github submodules to create component based designs.
+*/
+
+
+
+
+
+
+
+// Concept 4: Origins of JavaScript
+/*
+ Creator: Brendan Eich
+*/
+
+
+
+
+
+// Concept 5: EcmaScript 
+/*
+ - Spec for Ecma languages like Javascript.
+ - MDN breaks down into human readable format.
+*/
+
+
+// Concept 6: JavaScript Language
+/*
+ - It is a prototype based language.
+ - Single threaded non blocking asynchronous concurrent language. I have a call-stack, an event loop, a callback queue and some APIs.
+ 
+ Tip:
+ - Watch js conf to learn more about Javascript.
+*/
+
+
+// Concept 7: JavaScript Engine
+/*
+  - A program that executes javascript code.
+  - Just in time complition: to have sense of future makes v8 more faster.
+*/
+
+
+
+// Concept 8: Javascript data types
+/*
+ Primitive
+  - String
+  - Number
+  - BigInt
+  - Boolean
+  - Undefined
+  - Null
+  - Symbol
+  
+
+Non Primitive
+  - Object (godfather of all objects in Js)
+  - Arrays
+  
+Notes:
+Everything is written in Caps because these are the functions for blueprint for all data we see.
+  
+*/
+
+// typeof | instanceof to determine type of data
+
+typeof "abs"; // "string"
+typeof 122334; // "number"
+typeof true; // "boolean"
+typeof NaN; // "number" 
+typeof null; // "object" oops!
+
+typeof {}; // "object"
+typeof []; // "object"
