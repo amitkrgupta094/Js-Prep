f5844daa8282869a86ac68bf3f96f4fe9835a42b
Amit Gupta <33304344+Amilight@users.noreply.github.com>
2021-06-28 03:10:21 +0530

    Js-recent parts intro section notes added

---
+++ Js_recent_parts_01.js
@@ -0,0 +1,56 @@
+// Section 1: Recent parts
+// Deck: 1-5
+// Lecture 1 - Javascript new features process
+/*
+ Javascript Fatigue?
+ 
+ - From 1999-2009 there were not features added to Js
+ - After IE6 - nothing updated for this browser made new browser
+ came into picture like firefox, chrome.
+ - They agreed on having Single Js.
+ - Late 2015:
+   - Javascript update came, 300+ new features were added.
+   - 2016 updates: Es2016
+   - ..
+   - 2019 updates: Es2019
+ - Process for Javascript features
+ 
+  
+*/
+
+
+// Lecture 2 - Declaritive Javascript
+/*
+ - Js moving towards more declaritive methods. Focus is more on what.
+ more declarative code communicates better.
+ - Old js was on imperative way of doing code.
+*/
+
+
+// Lecture 3 - Browser Support & Transpilers
+/*
+ What about browser support?
+ - Transpilers (Babel) takes your code in new stanrd Js and creates them into older form of js so it can run everywhere.
+*/
+
+
+// Lecture 4 - Course overview
+/*
+ Javascript versions:
+ - ES6/ Es2015
+ - ES2016
+ - ES2017
+ - ES2018
+ - ES2019
+ 
+ Features:
+  - Template Strings
+  - String padding/trimming
+  - Destructing
+  - Array find() / includes()
+  - Array flat() / flatMap()
+  - Iterators, Generators
+  - RegExp Improvements
+  - async..await
+  - async* ..yield await
+*/
