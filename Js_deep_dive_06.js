8d5f0ace988feea90c58b2dc4e4359bac13369cb
Amit Gupta <33304344+Amilight@users.noreply.github.com>
2021-06-27 16:07:13 +0530

    Create Js_deep_dive_06.js
    Section 6: Type aware system in Js

---
+++ Js_deep_dive_06.js
@@ -0,0 +1,82 @@
+// Section 6: TypeSystems: TypeScript & Flow
+
+// Lecture 1: TypeScript, Flow and type-aware linting
+
+/*
+  - Linting: A tool to apply opinion about the code.
+  - A team should have style guide and everyone should aligned with it.
+  
+  TypeScript | Flow
+  - Kyle says he does not use it because he already solved it using vanilla js.
+  - Benefits:
+    - Catch type-related mistakes.
+    - Communicate type intent.
+    - Provide IDE feedback.
+  - Caveats
+    - Inferencing is best-guess. not a gurantee.
+    - Annotations are optional.
+    - Any part of the application that isn't types introduces uncertainty.
+  Deck: 131-133
+*/
+
+
+
+// Lecture 2 - Inferencing
+/*
+- By default ts and flow to auto inferencing.
+- Type aware linting
+
+Deck: 134-135
+*/
+
+
+// Lecturr 3 - Custom types
+/*
+- We can define our own custom types.
+Deck: 136
+*/
+
+
+// Lecture 4 - Validating Operand Types
+/*
+ - Type-Aware linting: It validates operand types. (ex: don't subtract from a string)
+ Deck: 137
+*/
+
+
+
+// Lecture 5: TS vs Flowtype
+/*
+Check this - https://github.com/niieani/typescript-vs-flowtype
+Deck: 138
+*/
+
+
+// Lecture 6: Ts & Flow Summary
+/*
+ Deck: 139-143
+ Pros:
+   - They make types more obvious in code.
+   - Familiarity: they look like other language's type system.
+   - Extremely popular these days.
+   - They're very sophisticated and good at what they do.
+*/
+
+
+// Lecture 7: Ts & Flow Summary
+/*
+ Deck: 144-149
+ Cons:
+   - Non-js standard syntax. [Creates system locking]
+   - It require build process so it raises the barrier to entry.
+   - Sophistication can be intimidating to without formal types experience.
+   - the focus more on "static types" than value types.
+*/
+
+
+// Lecture 8: Understanding your types
+/*
+Deck: 150-162
+Check Typl: https://frontendmasters.com/courses/deep-javascript-v3/bonus-typl/
+
+*/
