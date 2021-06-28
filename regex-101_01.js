70116411414d84583e21d7adb70a1a0f42f02f46
Amit Gupta <33304344+Amilight@users.noreply.github.com>
2021-06-28 15:22:51 +0530

    Section 1: topics sheet added

---
+++ regex-101_01.js
@@ -0,0 +1,17 @@
+// Section 1 - Intro
+// Lecture 1 - Topics to learn
+/*
+ Topics -
+ - Basics - History of Regular Expression
+ - Different ways to use them in JavaScript
+ - Testing Regular Expression
+ - Defining Patterns
+ - Metacharacters
+ - Character Sets
+ - Repetition
+ - Groupings
+ - Anchrored Expression
+ - Lookahead Assertion
+ - Using Unicode
+ - Useful Regular Expressions
+*/
