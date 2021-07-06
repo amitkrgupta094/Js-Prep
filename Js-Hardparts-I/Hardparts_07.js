aebf0400549a09ac008f9c18f432039b0f6ba8ef
Amit Gupta <33304344+Am1tgupt4@users.noreply.github.com>
2021-07-07 04:54:13 +0530

    wrap up

---
+++ Js-Hardparts-I/Hardparts_07.js
@@ -0,0 +1,9 @@
+// Section 7: Wrapping Up
+
+/*
+ - Principles of JavaScript.
+ - Callbacks & Higher order functions
+ - Closure (scope and execution context)
+ - Asynchronous JavaScript & the event loop
+ - Classes & Prototypes (OOP)
+*/
