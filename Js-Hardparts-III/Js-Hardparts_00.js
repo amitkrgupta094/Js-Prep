bfa9bbbd38025c117899284a1b10cc7b6bbbda58
Amit Gupta <33304344+Am1tgupt4@users.noreply.github.com>
2021-07-08 23:58:04 +0530

    Section 1 added

--- Js-Hardparts-III/Js-Hardparts_00.js
+++ Js-Hardparts-III/Js-Hardparts_00.js
@@ -1,1 +1,27 @@
-// Test
+// Section 1 - Introduction
+
+
+
+// Lecture 1: Course Introduction
+/*
+ - Coding Smith is run by Mr. Will Sentance [CEO]
+ - What Will looks in Software Engineer:
+   - Analytical Problem solving with code.
+   - Technical Communication
+   - Best practices: Debugging, Code Structure, patience and documentation.
+  - Non-technical communication.
+  - Language and computer science experience.
+*/
+
+
+// Lecture 2: Object Oriented Paradigm
+/*
+ OOP:
+ - It is a paradigm to structure our complex code.
+ - Easy to add features and functionality.
+ - Easy for us and other developers to reason about (a clear structure)
+ - Performant (efficient in terms of memory)
+ 
+ Paradigm: Any approach to organise our code [ In context of coding]
+ Object: data + functionality.
+*/
