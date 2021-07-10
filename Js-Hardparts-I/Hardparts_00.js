7081094ca03943aa4dc35283a7e36c1b3bdd3902
Amit Gupta <33304344+Am1tgupt4@users.noreply.github.com>
2021-07-11 05:02:35 +0530

    Notes updated

--- Js-Hardparts-I/Hardparts_00.js
+++ Js-Hardparts-I/Hardparts_00.js
@@ -1,18 +1,17 @@
-  ## JavaScript Hard Parts :![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 1")
-  
-  
-  ### Topics:
+ /*
+ JavaScript Hard Parts:
+  # Topics:
   - JavaScript Principles (How Js runs its code)
   - Callbacks & Higher Order functions
   - Closure
   - OOP: Classes/Prototypes & Asynchronity
   
   
-  ### Why understanding JS deep is important?
+  # Why understanding JS deep is important?
   - You would able to build things using knowledge of atoms. 
   
   
-  ### Different Levels of Engineers & Technical Communication:
+  # Different Levels of Engineers & Technical Communication:
   - Jr Level Engineer: 
     - If they are given a problem and have seen the same tech
     They will solve it.
@@ -22,3 +21,4 @@
    
   - Senior Level Engineer:
     - Takes a problem, solves it and also enables member of team to understand it and expand on it. [Technical Communication is important.]  
+*/
