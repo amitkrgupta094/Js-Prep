4092eed1e7e9ea22653f081b3e68307da6f5904e
Amit Gupta <33304344+Am1tgupt4@users.noreply.github.com>
2021-07-10 11:42:59 +0530

    Section I: Introduction added

--- Js-Hardparts-III/Js-Hardparts_00.js
+++ Js-Hardparts-III/Js-Hardparts_00.js
@@ -4,24 +4,48 @@
 
 // Lecture 1: Course Introduction
 /*
- - Coding Smith is run by Mr. Will Sentance [CEO]
- - What Will looks in Software Engineer:
-   - Analytical Problem solving with code.
-   - Technical Communication
+ # Author's Bio:
+   - CodingSmith is run by Mr. Will Sentance [CEO]
+   - What Will looks in a Software Engineer:
+ 
+ # Things we look in an Enginner?
+   - Analytical Problem solving with code. [Hitting a block and solve it.]
+   - Technical Communication 
+      - It means Can I implement your approach just from explanation?
+      - Verbal communication enabling other to implement a feature.
    - Best practices: Debugging, Code Structure, patience and documentation.
-  - Non-technical communication.
-  - Language and computer science experience.
+   - Non-technical communication.
+   - Language and computer science experience.
+   
 */
 
 
 // Lecture 2: Object Oriented Paradigm
 /*
- OOP:
- - It is a paradigm to structure our complex code.
- - Easy to add features and functionality.
- - Easy for us and other developers to reason about (a clear structure)
- - Performant (efficient in terms of memory)
+ # Syllabus: 
+   1. Number of ways we can produce objects (data + functionality bundled up nicely.)
+   2. Deep on call and apply.
+   3. How prototype chain plays out
+   4. Subclasses 
+     I. Factory Function Style
+     II. new Keyword Style
+     III. new ES2015 Class Style
  
- Paradigm: Any approach to organise our code [ In context of coding]
- Object: data + functionality.
+ 
+ # But.. why even we have this way of coding?
+   - A code is all about data and applying functionality on it.
+     Would not it be nice if we can bundle data and functionalaties 
+     together instead of having code here and there?
+ 
+ 
+ # OOP:
+   - It is a paradigm to structure our complex code.
+   - Easy to add features and functionality.
+   - Easy for us and other developers to reason about (A clear structure).
+   - Performant (Efficient in terms of memory).
+ 
+ 
+ $ Jargons: 
+   - Paradigm: Any approach to organise our code.[In context of coding]
+   - Object: data + functionality.
 */
