56897a5d75f562f6a8421c86f47d0a9f6e69b745
Amit Gupta <33304344+Am1tgupt4@users.noreply.github.com>
2021-07-25 07:25:30 +0530

    Added stuffs for notes_01

--- YDKJS-Series[Notes]/Book1[Get-Started]/Notes_01.js
+++ YDKJS-Series[Notes]/Book1[Get-Started]/Notes_01.js
@@ -1,1 +1,31 @@
-// Added the dummy text here
+// Why I'm reading this book?
+/*
+  1. I want to build great web products/libraries/plugins which needs deep knowledge 
+     of underlying principles of Js and Programming in General.
+  2. I want to earn more [aka crack a good company] as a developer and have a decent living.
+
+*/
+
+
+// Forward by Brian Holt (Currently working on Stripe)
+/*
+  - Brian great forward about the book. I have learnt a many new words from here:
+    1. Get bent
+    2. Sweeping Claim
+    3. Knack for
+    4. Zeitgeist 
+    5. Sway
+    6. Grapples
+    7. Underpinnings
+    
+    My Favourite Quote: 
+    ```
+    So, do your future self a favor and dig into this book and unlock the knowledge within.
+    These solid foundations will serve you better than any framework ever will;
+    those come and go but we'll still be writing JavaScript itself for decades to come.
+    Keep an open mind and challenge your preconceived notions
+    ```
+*/
+
+
+
