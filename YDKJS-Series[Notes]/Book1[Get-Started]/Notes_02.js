0201df612f2654bb4059ca0bdf9232335a614b4f
Amit Gupta <33304344+Am1tgupt4@users.noreply.github.com>
2021-07-25 07:27:55 +0530

    Preface added for Js Book

---
+++ YDKJS-Series[Notes]/Book1[Get-Started]/Notes_02.js
@@ -0,0 +1,7 @@
+// Preface by Kyle Simpson (One of the ground breaker in JS-land)
+/*
+  - He talks about why it is important for a developer
+    to learn under the hood stuffs to write more communicative 
+    code which lean to more maintainable product and code.
+  - Read everything here: https://github.com/getify/You-Dont-Know-JS/blob/2nd-ed/preface.md
+*/
